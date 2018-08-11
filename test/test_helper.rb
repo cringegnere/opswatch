4a079b8139e3688993d35b18dbecb34a28b93100
Gianluca Rizzo <gianluca.rizzo@keypartner.it>
2018-08-11 14:47:41 +0200

    Add ci_reporter gem for coverage reporting (#10)
    * Add ci_reporter gem
    
    * Add simplecov
    
    * Add coverall support
    
    * Add coverage badge

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -2,5 +2,9 @@ $LOAD_PATH.unshift File.expand_path("../../lib", __FILE__)
 GEM_ROOT = File.expand_path("../..", __FILE__)
 
 require "opswatch"
-
 require "minitest/autorun"
+require 'simplecov'
+require 'coveralls'
+
+Coveralls.wear!
+SimpleCov.start
