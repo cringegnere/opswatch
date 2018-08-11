f91079e5ef1137f53263351d4e8c04e3da799d2c
Gianluca Rizzo <gianluca.rizzo@keypartner.it>
2018-08-11 09:08:44 +0200

    Add simplecov

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -2,5 +2,7 @@ $LOAD_PATH.unshift File.expand_path("../../lib", __FILE__)
 GEM_ROOT = File.expand_path("../..", __FILE__)
 
 require "opswatch"
-
 require "minitest/autorun"
+require 'simplecov'
+
+SimpleCov.start
