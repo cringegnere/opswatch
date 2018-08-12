ef7664c41039de07ff9daf10a303b3ddf2e09f10
Gianluca Rizzo <gianluca.rizzo@keypartner.it>
2018-08-12 10:46:58 +0200

    Fix coverage calculation

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,11 +1,13 @@
 require 'coveralls'
+require 'simplecov'
 
 $LOAD_PATH.unshift File.expand_path("../../lib", __FILE__)
 GEM_ROOT = File.expand_path("../..", __FILE__)
 
+SimpleCov.formatter = Coveralls::SimpleCov::Formatter
+SimpleCov.start do
+  add_filter "/test/"
+end
+
 require "opswatch"
 require "minitest/autorun"
-require 'simplecov'
-
-SimpleCov.formatter = Coveralls::SimpleCov::Formatter
-SimpleCov.start
